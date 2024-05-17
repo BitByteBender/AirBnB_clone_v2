f0f26977b37e2a040e787d91a2b392287cf8e503
GodSlayerMode <ssadgali2@gmail.com>
2024-05-17 22:21:33 +0100

    Test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,15 @@ web_flask = Flask(__name__)
 
 
 @web_flask.route('/', strict_slashes=False)
-def Hello():
-    """ Returns a string as a response """
+def hbnb():
+    """
+        Returns a string as a response
+    """
     return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    """ Runs the Flask app """
+    """
+        Runs the Flask app
+    """
     web_flask.run(host='0.0.0.0', port=5000)
