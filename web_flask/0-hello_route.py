a7a740703e1710bd49b474c94ab1e53f65141be6
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-17 12:44:38 -0700

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!usr/bin/python3
-
+# Import Flask class from flask module
 from flask import Flask
-
+# Flask app instance creation
 web_flask = Flask(__name__) 
 
 
+# Defining a route for the root URL (/)
 @web_flask.route('/', strict_slashes=False)
 def Hello_Flask():
+    """ Returns a string as a response """
     return ("Hello HBNB!")
 
 
 if (__name__ == "__main__"):
+    # Runs the Flask app
     web_flask.run(host="0.0.0.0", port=5000)
