90f6afa1c3ef75a3bc6f747853478b496b6e52ed
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-18 16:59:14 -0700

    Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,9 +15,9 @@ def teardown_db(exception):
 @app.route("/states_list", strict_slashes=False)
 def list_of_states():
     """ Renders an HTML page """
-    sts = sorted(storage.all(State).values(),
+    states = sorted(storage.all(State).values(),
                     key=lambda x: x.name)
-    return (render_template("7-states_list.html", sts=sts))
+    return (render_template("7-states_list.html", states=states))
 
 
 if __name__ == "__main__":
