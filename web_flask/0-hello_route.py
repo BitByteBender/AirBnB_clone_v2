f0a195bc4d9becf173877cb0e57ea8fd234bfb96
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-18 08:08:54 -0700

    Test req

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,19 +4,19 @@
     Flask app instance creation
 """
 from flask import Flask
-web_flask = Flask(__name__)
+app = Flask(__name__)
 
 
-@web_flask.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def main():
     """
         Returns a string as a response
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
         Runs the Flask app
     """
-    web_flask.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
